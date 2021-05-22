191a2df7d97faf8cb26d970d855189b61b65dcc7
Sergei Irin <v-seirin@microsoft.com>
2021-05-22 15:58:57 +0300

    UI app initiated

--- App/misiks/urls.py
+++ App/misiks/urls.py
@@ -1,7 +1,9 @@
+from ui.views import index
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from api import views
+import ui
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -14,7 +16,8 @@ router.register(r'requirements', views.RequirementViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    path('', ui.views.index),
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
