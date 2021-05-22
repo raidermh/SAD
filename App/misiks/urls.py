6c609e9b89d0e6f298175b0545c6cafb0bb8ab0c
Sergei Irin <v-seirin@microsoft.com>
2021-05-22 21:39:47 +0300

    Main menu and login/logout implemented

--- App/misiks/urls.py
+++ App/misiks/urls.py
@@ -1,23 +1,27 @@
-from ui.views import index
 from django.contrib import admin
-from django.urls import include, path
+from django.contrib.auth import views as auth_views
+from django.urls import include, path, re_path
 from rest_framework import routers
-from api import views
-import ui
+from api import views as api_views
+from ui import views as ui_views
 
+# API router
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'projects', views.ProjectViewSet)
-router.register(r'roles', views.RoleViewSet)
-router.register(r'specifications', views.SpecificationViewSet)
-router.register(r'releases', views.ReleaseViewSet)
-router.register(r'requirements', views.RequirementViewSet)
+router.register(r'users', api_views.UserViewSet)
+router.register(r'projects', api_views.ProjectViewSet)
+router.register(r'roles', api_views.RoleViewSet)
+router.register(r'specifications', api_views.SpecificationViewSet)
+router.register(r'releases', api_views.ReleaseViewSet)
+router.register(r'requirements', api_views.RequirementViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', ui.views.index),
+    path('', ui_views.projects),
+    path('releases/', ui_views.releases),
+    re_path(r'(.*)logout/$', ui_views.logout_view),
     path('admin/', admin.site.urls),
+    path('accounts/login/', auth_views.LoginView.as_view()),
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- App/ui/views.py
+++ App/ui/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 
-def index(request):
+def logout_view(request, _):
+    logout(request)
+    return redirect(request.build_absolute_uri('/accounts/login/?next=/'))
+
+
+@login_required
+def projects(request):
     return render(request, 'projects.html')
+
+
+@login_required
+def releases(request):
+    return render(request, 'releases.html')
